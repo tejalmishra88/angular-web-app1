2d9080939352cfe8f7767c20467569bad2a5f78f
tejalmishra88 <tejalmishra88@gmail.com>
2018-04-08 17:11:06 +1000

    using id

---
+++ src/app/post/post.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  id: number=0;
+    post={};
+  constructor( private route: ActivatedRoute) {
+    this.route.params.subscribe(res => this.id = res.id);
+    fetch("https://jsonplaceholder.typicode.com/posts/"+this.id)
+    .then((res) => res.json())
+    .then((data) => this.post =data)
+    .catch((err) => console.log(err))
+   }
+
+  ngOnInit() {
+  }
+
+}
