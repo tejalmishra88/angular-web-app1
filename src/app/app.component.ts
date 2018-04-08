7885e17275ff18ac7c35210b34a97a1c6ccfbf41
tejalmishra88 <tejalmishra88@gmail.com>
2018-04-08 11:13:11 +1000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+ // templateUrl: './app.newcomponent.html',
+// template: `<h2>tejal</h2>`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tejal';
+ // title = 'app';
+}
