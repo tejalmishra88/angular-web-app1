74129fae1fada634239f370e983a52f9cc7b4531
tejalmishra88 <tejalmishra88@gmail.com>
2018-04-08 14:24:41 +1000

    angular navigation  with router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+let routes: Routes= [
+  { path: "features",
+    component: FeaturesComponent
+  },
+  {
+    path: "pricing",
+    component: PricingComponent
+  }
+]
 
 @NgModule({
-  declarations: [    AppComponent, ],
-  imports: [    BrowserModule , FormsModule],
+  declarations: [    AppComponent, FeaturesComponent, PricingComponent, SidebarComponent, ],
+  imports: [    BrowserModule , FormsModule, RouterModule.forRoot(routes), ],
   providers: [],
   bootstrap: [AppComponent]
 })
