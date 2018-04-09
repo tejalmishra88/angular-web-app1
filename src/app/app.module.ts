400def5f8c1169ad736cab7edc3148af25bb2af5
tejalmishra88 <tejalmishra88@gmail.com>
2018-04-09 17:34:41 +1000

    create templates and component manually

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { PricingComponent } from './pricing/pricing.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostComponent } from './post/post.component';
+import { LoginComponent } from './login.component';
 let routes: Routes= [
   { path: "features",
     component: FeaturesComponent
@@ -25,11 +26,20 @@ let routes: Routes= [
   {
     path: "posts/:id",
     component: PostComponent
-  }
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
 ]
 
 @NgModule({
-  declarations: [    AppComponent, FeaturesComponent, PricingComponent, SidebarComponent, PostsComponent, PostComponent, ],
+  declarations: [    AppComponent, FeaturesComponent, PricingComponent, SidebarComponent,
+     PostsComponent, PostComponent,LoginComponent ],
   imports: [    BrowserModule , FormsModule, RouterModule.forRoot(routes,{ useHash: true}), ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,4 +1,31 @@
-.card-container.card {
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+ template:`<div class="container">
+ <div class="card card-container">
+     <!-- <img class="profile-img-card" src="//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120" alt="" /> -->
+     <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+     <p id="profile-name" class="profile-name-card"></p>
+     <form class="form-signin">
+         <span id="reauth-email" class="reauth-email"></span>
+         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+         <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+         <div id="remember" class="checkbox">
+             <label>
+                 <input type="checkbox" value="remember-me"> Remember me
+             </label>
+         </div>
+         <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">{{ title }}</button>
+     </form><!-- /form -->
+     <a href="#" class="forgot-password">
+         Forgot the password?
+     </a>
+ </div><!-- /card-container -->
+</div><!-- /container -->
+ `,
+ styles:[
+   `.card-container.card {
     max-width: 350px;
     padding: 40px 40px;
 }
@@ -126,4 +153,15 @@
 .forgot-password:active,
 .forgot-password:focus{
     color: rgb(12, 97, 33);
+}
+   `
+ ]
+ // templateUrl: './app.newcomponent.html',
+// template: `<h2>tejal</h2>`,
+ 
+})
+export class LoginComponent {
+    title = ' login form ';
+ // title = 'tejal';
+ // title = 'app';
 }
